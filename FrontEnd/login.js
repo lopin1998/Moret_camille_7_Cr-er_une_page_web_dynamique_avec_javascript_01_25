99566c4f4b1f25a8da627aaaf7e0722b0d6688e9
Moret Camille <moretcamille@orange.fr>
2025-01-10 13:26:33 +0100

    regex login fonctionnel

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,36 +4,36 @@ loginForm.addEventListener("submit", async (e) => {
     e.preventDefault()
     const email = loginForm.email.value
     const password = loginForm.password.value
-/* ajouter de verification pour email et pasword et ajouter une regex 101 pour email */
-    try {
-        let response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
+    let patern = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm
+    let result = patern.test(email)
+    /* ajouter de verification pour email et pasword et ajouter une regex 101 pour email */
+    if (result == true) {
+        try {
+            let response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
 
+                })
             })
-        })
-        if(response.status != 200){
-            alert("mot de passe ou email incorect")
-            throw new Error("coucou")
+            if (response.status != 200) {
+                alert("mot de passe ou email incorect")
+                throw new Error()
+            }
+            let data = await response.json()
+            localStorage.setItem("keyT", data.token)
+            window.location.href='index.html'
+        } catch (error) {
+
         }
-        let data = await response.json()
-        localStorage.setItem("keyT", data.token)
-        window.location.href='index.html'
-    } catch (error) {
-        
+    }
+    else{
+        alert("veuillez saisir une adresse mail conforme ex: test1234@test.fr")
     }
 
 })
-
-
-
-/* /
-^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$
-/
-gm */
